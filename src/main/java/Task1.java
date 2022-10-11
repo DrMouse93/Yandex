9a9b832ccb08059e37c1e5ae78b172b4d8d09ca8
Vadim <vadimshalin@mail.ru>
2022-10-11 14:04:18 +0300

    Innit commit

--- src/main/java/Task1.java
+++ src/main/java/Task1.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Task1 {
@@ -15,11 +15,23 @@ public class Task1 {
         secondLine = fillList(scanner);
         thirdLine = fillList(scanner);
 
-        System.out.println(secondLine.contains("2"));
-
         List<String> answerList;
 
-        answerList = new ArrayList<>(mergeLists(firstLine, secondLine));
+        int[] sizeInits = new int[6];
+
+        sizeInits[0] = initAnswerLists(firstLine, secondLine, thirdLine).size();
+        sizeInits[1] = initAnswerLists(secondLine, firstLine, thirdLine).size();
+        sizeInits[2] = initAnswerLists(firstLine, thirdLine, secondLine).size();
+        sizeInits[3] = initAnswerLists(thirdLine, firstLine, secondLine).size();
+        sizeInits[4] = initAnswerLists(secondLine, thirdLine, firstLine).size();
+        sizeInits[5] = initAnswerLists(thirdLine, secondLine, firstLine).size();
+
+        int minSize = Arrays.stream(sizeInits).filter(x -> x != 0).min().getAsInt();
+
+        System.out.println(minSize);
+
+
+        /*answerList = new ArrayList<>(mergeLists(firstLine, secondLine));
         if (answerList.isEmpty()) {
             answerList = new ArrayList<>(mergeLists(secondLine, thirdLine));
             answerList = new ArrayList<>(mergeLists(answerList, firstLine));
@@ -30,10 +42,17 @@ public class Task1 {
         if (answerList.isEmpty()) {
             answerList = new ArrayList<>(mergeLists(firstLine, thirdLine));
             answerList = new ArrayList<>(mergeLists(answerList, secondLine));
-        }
+        }*/
 
 
-        System.out.println(answerList);
+        //System.out.println(answerList);
+    }
+
+    private static List<String> initAnswerLists(List<String> firstList,
+                                                List<String> secondList, List<String> thirdList) {
+        List<String> tempAnswer = new ArrayList<>(mergeLists(firstList, secondList));
+        if (tempAnswer.isEmpty()) return new ArrayList<>();
+        return new ArrayList<>(mergeLists(tempAnswer, thirdList));
     }
 
     private static List<String> fillList(Scanner scanner) {
@@ -48,38 +67,39 @@ public class Task1 {
     }
 
     private static List<String> mergeLists(List<String> firstList, List<String> secondList) {
-        /*int firstCount = numberMatchingValues(firstList, secondList);
-        int secondCount = numberMatchingValues(secondList, firstList);*/
+        int firstCount = numberMatchingValues(firstList, secondList);
 
-        if (!numberMatchingValues(firstList, secondList).isEmpty()) {
-            firstList.addAll(numberMatchingValues(firstList, secondList));
+        if (firstCount > 0) {
+            firstList = firstList.subList(0, firstList.size() - firstCount);
+            firstList.addAll(secondList);
             return firstList;
-        } else if (!numberMatchingValues(secondList, firstList).isEmpty()) {
-            secondList.addAll(numberMatchingValues(secondList, firstList));
-            return secondList;
         }
 
         return new ArrayList<>();
     }
 
-    /*private static int numberMatchingValues(List<String> firstList, List<String> secondList) {
-        int count = 0;
+    private static int numberMatchingValues(List<String> firstList, List<String> secondList) {
+        if (firstList.size() == 0) return 0;
 
         int size = Math.min(firstList.size(), secondList.size());
 
         for (int i = 0; i < size; i++) {
-            if (firstList.get(firstList.size() - 1 - count).equals(secondList.get(count))) {
-                count++;
-            } else {
-                break;
-            }
+            if (!firstList.get(i).equals(secondList.get(i)))
+                return numberMatchingValues(firstList.subList(1, firstList.size()), secondList);
         }
-         return count;
-    }*/
 
-    private static List<String> numberMatchingValues(List<String> firstList, List<String> secondList) {
-        if (secondList.size() == 0) return new ArrayList<>();
-        return firstList.contains(secondList) ? secondList :
-                numberMatchingValues(firstList, secondList.subList(0, secondList.size() - 1));
+        return firstList.size();
     }
+
+    /*private static List<String> numberMatchingValues(List<String> firstList, List<String> secondList) {
+        if (secondList.size() == 0) return new ArrayList<>();
+
+        int size = Math.min(firstList.size(), secondList.size());
+
+        for (int i = 0; i < size; i++) {
+            if (!firstList.get(firstList.size() - 1 - i).equals(secondList.get(i)))
+                return numberMatchingValues(firstList.subList(1, firstList.size()), secondList);
+        }
+        return secondList;
+    }*/
 }
